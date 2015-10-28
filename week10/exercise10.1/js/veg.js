918bec3dfdb3576308df34d863d181f03ac68b0e
Tony Peruzzo <anthony_peruzzo@hotmail.com>
2015-10-28 08:48:59 -0500

    exercise 10.2

--- week10/exercise10.1/js/veg.js
+++ week10/exercise10.1/js/veg.js
@@ -1,3 +1,17 @@
 $("document").ready(function (){
+	//adding v to vegetatian options in room service page
 	$('li[data-type="veg"]').append(' (v)');
+
+	//select h2 elements
+	var specials = $('.content h2');
+	//create ul element
+	$('.content h1').after('<ul></ul>');
+
+	specials.each(function(i){
+		//create unique id for each special h2
+		$(this).attr('id', 'special' + i);
+		var headerText = $(this).text();
+		//create an li and a element that links to cooresponding special h2
+		$('.content ul').append('<li><a href="#special' + i + '">' + headerText + '</a></li>');
+	});
 });
