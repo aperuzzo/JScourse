ebd824295e07fb95da2178bf7ce1fd33c3415406
Tony Peruzzo <anthony_peruzzo@hotmail.com>
2015-11-09 15:01:47 -0600

    week12
    exercise

--- week10/exercise10.1/js/veg.js
+++ week10/exercise10.1/js/veg.js
@@ -3,11 +3,11 @@ $("document").ready(function (){
 	$('li[data-type="veg"]').append(' (v)');
 
 	//select h2 elements
-	var specials = $('.content h2');
+	var $specials = $('.content h2');
 	//create ul element
 	$('.content h1').after('<ul></ul>');
 
-	specials.each(function(i){
+	$specials.each(function(i){
 		//create unique id for each special h2
 		$(this).attr('id', 'special' + i);
 		var headerText = $(this).text();
@@ -21,5 +21,17 @@ $("document").ready(function (){
 		$(this).children('ul').toggleClass('red');
 	});
 
+	setInterval('rotateImages()', 4000);
+	
 });
 
+function rotateImages(){
+	var $current = $('#hero div.current');
+	var $next = ($current.next().length > 0) ? $current.next() : $('#hero div:first');
+
+	$current.removeClass('current').addClass('previous');
+	$next.css('opacity', 0).addClass('current').animate({opacity: 1}, 1300, function(){
+		$current.removeClass('previous');
+
+	})
+}
